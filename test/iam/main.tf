9e67f9178f8b6cee71cd2c192e98efd5df9ddb59
Tarlok <tarlok_chand@crgl-thirdparty.com>
2025-03-21 12:46:51 +0530

    destroy code

--- test/iam/main.tf
+++ test/iam/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
     //version = "~> 2.46" (No longer necessary)
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tarlok-s3-tes111" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+
 resource "aws_iam_user" "my_iam_user" {
     name = "my_iam_user"
 }
